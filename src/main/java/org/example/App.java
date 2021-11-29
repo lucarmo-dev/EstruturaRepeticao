a1f8a98fce59a0e38e5200897f6a221309964fa0
lucarmo-dev <carmo.lucas.lc@gmail.com>
2021-11-29 20:46:03 -0300

    First Commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,42 @@
+package org.example;
+import java.util.Scanner;
+
+public class App
+{
+
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um numero correpondente ao dia da semana: ");
+        int num = sc.nextInt();
+        String dia = null;
+
+        switch (num){
+            case 1:
+                dia = "domingo";
+                break;
+            case 2:
+                dia = "segunda";
+                break;
+            case 3:
+                dia = "terça";
+                break;
+            case 4:
+                dia = "quarta";
+                break;
+            case 5:
+                dia = "quinta";
+                break;
+            case 6:
+                dia = "sexta";
+                break;
+            case 7:
+                dia = "sabado";
+                break;
+
+        }
+
+        System.out.println("O dia da semana escolido e: " + dia);
+        
+    }
+}
